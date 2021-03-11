b314987f618a7fa7f248e38a7937627755c743c9
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-03-11 15:52:15 +0100

    Fix

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ var hamburgerMenu = $('.hamburger-menu'),
     openClose = $('.close, .header-right > a');
 
 openClose.click( function(){
-  hamburgerMenu.toggle('active');
+  hamburgerMenu.toggle('slow');
 });
